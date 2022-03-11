e4c1bd5a6f1ba3d3015e4f5dd127b1c1cede9a48
Francisco Silva <31171097+franciscomsilva96@users.noreply.github.com>
2022-03-11 15:16:55 +0000

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,7 +1,12 @@
-var ss = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("OBRAS");
+// The use case of this script is to identify which references in our dataset have finish date within 4 weeks or less.
+// It will set a popup message everytime we open the spreadsheet with the name of the references that match the criteria.
+// Still working on trying to display all of the references that match the criteria in a single popup message.
+
+var ss = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("name of the sheet we want");
 var startRow = 2;
 var startColumn = 15;
 
+// Function to get the last row with data within a range
 function getLastActiveRow(range) {
   var rowNum = 0;
   var blank = false;
@@ -18,19 +23,17 @@ function getLastActiveRow(range) {
   return rowNum;
 };
 
+
 function onOpen(e){
 
-  //Com estas duas variáveis vamos buscar o nº da última linha com células preenchidas para não 
-  //irmos buscar todas as linha do dataset.
-
-    //Select the column we will check for the first blank cell
+  // With these two variables we will check for the last cell with data in a test column
+    // Select the column we will check for the first blank cell - in my case that is column C
     var columnToCheck = ss.getRange("C2:C").getValues();
     
     // Get the last row based on the data range of a single column.
     var endRow = getLastActiveRow(columnToCheck);
 
-  //Neste ciclo for vamos iterar sobre a coluna das datas de colocação
-  //e transformar os valores no formate Date.
+  // Iterate through the column with date values and transform them in Date format
   for (var i = startRow; i < ( endRow + 1 ); i++) {
     
     var startDate = new Date();
@@ -40,9 +43,9 @@ function onOpen(e){
     var year = ddl.substring(6, 10);
     var month = ddl.substring(0, 2);
     var day = ddl.substring(3, 5);
-    var deadline = new Date(year, month - 1, day); //datas de colocação em formato Date
+    var deadline = new Date(year, month - 1, day); //datas em formato Date
     
-    //Calcular o nº de semanas entre a data de hoje e a data de colocação
+    // Calculate number of weeks between our range
     var semanas = parseInt(((deadline - startDate) / 1000 / 60 / 60 / 24 / 7) + 1);
     //var newRange = ss.getRange("Z2:Z").setValues(semanas);
 
@@ -69,10 +72,11 @@ function onOpen(e){
 
     }*/
 
+    // Set popup for the rows with semanas <= 4
     if(semanas <= 6) {
       if(semanas >= 0) {
         var ref = ss.getRange(i,4).getValues();
-        SpreadsheetApp.getUi().alert("A obra " + ref + " tem prazo de entrega dentro de " + semanas + " semanas.");
+        SpreadsheetApp.getUi().alert("A referência " + ref + " tem prazo de entrega dentro de " + semanas + " semanas.");
       }
     }
   } 
